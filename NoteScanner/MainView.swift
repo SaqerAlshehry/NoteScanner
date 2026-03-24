c0f6938a23cdfb06e64c55b7b451fa654b550081
Saqer Alshehri <riche.homme.10@gmail.com>
2026-03-24 16:32:41 +0300

    first commit

---
+++ NoteScanner/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  NoteScanner
+//
+//  Created by Saqer Alshehri on 05/10/1447 AH.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Note Scanner Project using AVFoundation Framework!")
+                .font(.system(size: 20, weight: .semibold, design: .monospaced))
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ NoteScanner/NoteScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteScannerApp.swift
+//  NoteScanner
+//
+//  Created by Saqer Alshehri on 05/10/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct NoteScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
