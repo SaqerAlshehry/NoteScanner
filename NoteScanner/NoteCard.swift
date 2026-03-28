9b020145e3c88f8d8a5af06d536bb0a9348b38b3
Saqer Alshehri <riche.homme.10@gmail.com>
2026-03-28 06:47:35 +0300

    note card created

--- NoteScanner/NoteCard.swift
+++ NoteScanner/NoteCard.swift
@@ -4,4 +4,33 @@
 //
 //  Created by Saqer Alshehri on 09/10/1447 AH.
 //
-
+import SwiftUI
+struct NoteCard : View {
+    var date : Date
+    var content : String
+    var body : some View {
+        RoundedRectangle(cornerRadius : 20)
+            .fill(.ultraThinMaterial)
+            .frame(width : 300, height : 90)
+            .overlay(alignment : .leading){
+                VStack(spacing : 8){
+                    Text(date.formatted())
+                        .font(.system(size : 12,weight: .bold,design: .monospaced))
+                        .foregroundColor(.secondary)
+                    Text(content)
+                        .font(.system(size : 14,weight: .bold,design: .monospaced))
+                        .foregroundColor(.primary)
+                        .lineLimit(1)
+                    Spacer()
+                }
+                .padding()
+            }
+            .overlay{
+                RoundedRectangle(cornerRadius :  20)
+                    .stroke(.black.opacity(0.15), lineWidth: 1)
+            }
+    }
+}
+#Preview{
+    NoteCard(date : Date(), content: "Note Content Note Content Note Content Note Content Note Content Note Content Note Content Note Content Note Content")
+}
